73be7bd24631bc8aa4732c16a33d8a8ec17cbe67
Dave <dave.cadelina@outlook.com>
2021-11-26 15:01:24 +1100

    first implementation of bubble sort

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ class Sorter {
     constructor(public collection: number[]) {}
 
     sort(): void {
+        const { length } = this.collection;
 
+        for(let  i = 0; i<length; i++) {
+            for(let j = 0; j<length-i-1; i++) {
+                if(this.collection[j] > this.collection[j+1]){
+                    const temp = this.collection[j]
+                    this.collection[j] = this.collection[j+1]
+                    this.collection[j+1] = temp
+                }
+            }
+        }
     }
 
 }
