684fe576fab0754960993ebd48e1a7b5d7c60c3e
Dave <dave.cadelina@outlook.com>
2021-11-26 23:13:01 +1100

    bad approach for type checking

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 class Sorter {
-    constructor(public collection: number[]) {}
+    constructor(public collection: number[] | String) {}
 
     sort(): void {
         const { length } = this.collection;
 
         for(let  i = 0; i<length; i++) {
             for(let j = 0; j<length-i-1; i++) {
+
+
+                //-- if collection is arry
                 if(this.collection[j] > this.collection[j+1]){
                     const temp = this.collection[j]
                     this.collection[j] = this.collection[j+1]
                     this.collection[j+1] = temp
                 }
+
+                //-- if collection is a string, do this
+                //-- compare and swap in string
             }
         }
     }
