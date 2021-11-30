515701f83a1284dbb1cae1a7ed1f5e13af4d62fc
Dave <dave.cadelina@outlook.com>
2021-11-30 23:47:24 +1100

    added type guard

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 class Sorter {
+
     constructor(public collection: number[] | String) {}
 
     sort(): void {
@@ -7,13 +8,15 @@ class Sorter {
         for(let  i = 0; i<length; i++) {
             for(let j = 0; j<length-i-1; i++) {
 
-
-                //-- if collection is arry
-                if(this.collection[j] > this.collection[j+1]){
-                    const temp = this.collection[j]
-                    this.collection[j] = this.collection[j+1]
-                    this.collection[j+1] = temp
+                if(this.collection instanceof Array) {
+                    //-- union restriction is now lifted
+                    if(this.collection[j] > this.collection[j+1]){
+                        const temp = this.collection[j]
+                        this.collection[j] = this.collection[j+1]
+                        this.collection[j+1] = temp
+                    }
                 }
+                
 
                 //-- if collection is a string, do this
                 //-- compare and swap in string
