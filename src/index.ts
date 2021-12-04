52b912b6dabb2c0132300119ac88aeb8a0e546e8
Dave <dave.cadelina@outlook.com>
2021-12-05 03:00:23 +1100

    intermeditate refactor before creating interface

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,5 @@
-class Sorter {
 
-    constructor(public collection: number[] | String) {}
 
-    sort(): void {
-        const { length } = this.collection;
-
-        for(let  i = 0; i<length; i++) {
-            for(let j = 0; j<length-i-1; i++) {
-
-                if(this.collection instanceof Array) {
-                    //-- union restriction is now lifted
-                    if(this.collection[j] > this.collection[j+1]){
-                        const temp = this.collection[j]
-                        this.collection[j] = this.collection[j+1]
-                        this.collection[j+1] = temp
-                    }
-                }
-                
-
-                //-- if collection is a string, do this
-                //-- compare and swap in string
-                if(typeof this.collection === 'string') {
-                    
-                }
-            }
-        }
-    }
-
-}
-
-const sorter = new Sorter([10, 3, -5, 0])
-sorter.sort();
-console.log(sorter.collection)
+// const sorter = new Sorter([10, 3, -5, 0])
+// sorter.sort();
+// console.log(sorter.collection)
