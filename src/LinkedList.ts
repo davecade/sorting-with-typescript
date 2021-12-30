76126ae5d167985862302b9ac08abdac428cc115
Dave <dave.cadelina@outlook.com>
2021-12-30 11:51:07 +1100

    added the add method to linkedlist

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -6,7 +6,23 @@ class Node {
 export class LinkedList {
     head: Node | null = null;
 
-    constructor(head: Node){
-        this.head = head
+    constructor(head: Node) {
+        this.head = head;
+    }
+
+    add(data: number): void {
+        const node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            return;
+        } else {
+            //-- Iterate to last node, then add node
+            let pointer = this.head;
+            while (pointer.next) {
+                pointer = pointer.next;
+            }
+
+            pointer.next = node;
+        }
     }
 }
